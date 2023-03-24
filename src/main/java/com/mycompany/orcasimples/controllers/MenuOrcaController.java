3c0b9ccc3bf3a38b897f203e27e23f670a3eae5e
Liedson <liedsonreis2019@outlook.com>
2023-03-24 16:06:09 -0300

    Ligação da janela de cadastro de insumos simples e embalagens com o menu

--- src/main/java/com/mycompany/orcasimples/controllers/MenuOrcaController.java
+++ src/main/java/com/mycompany/orcasimples/controllers/MenuOrcaController.java
@@ -2,13 +2,13 @@ package com.mycompany.orcasimples.controllers;
 
 import javafx.fxml.FXML;
 import com.mycompany.orcasimples.model.util.FxmlTools;
-import com.mycompany.orcasimples.model.util.FileTools;
 import java.io.IOException;
-import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public class MenuOrcaController {
 
@@ -37,11 +37,40 @@ public class MenuOrcaController {
     private Button btnShowSimpleFoodstock;    
     
     @FXML
-    private AnchorPane subScene;
+    private Pane subScene;            
+    
+    @FXML
+    void setSimpleFoodsctockCadScene(MouseEvent event){                                       
+        setSceneInSubScene("telaCadSimples");
+    }
+    
+    @FXML
+    void setCompoundFoodstockScene(MouseEvent event) {
+        setSceneInSubScene("telaCadCompoundFoodstock");
+    }
 
     @FXML
-    void setSimpleFoodsctockCadScene(MouseEvent event) throws IOException {                       
-        Parent node = FxmlTools.loadFXML("telaCad_simples");
-        subScene.getChildren().add(node);
+    void setPackagingCadScene(MouseEvent event) {
+        setSceneInSubScene("telaCadEmbalagem");
+    }
+
+    @FXML
+    void setProductCadScene(MouseEvent event) {
+        setSceneInSubScene("telaCadProduto");
+    }
+    
+    private void setSceneInSubScene(String sceneName) {
+        Parent node;                                                  
+        try {
+            
+            if (!subScene.getChildren().isEmpty()) {
+                subScene.getChildren().remove(0);
+            }
+            
+            node = FxmlTools.loadFXML(sceneName);
+            subScene.getChildren().add(node);
+        } catch (IOException ex) {
+            Logger.getLogger(MenuOrcaController.class.getName()).log(Level.SEVERE, null, ex);
+        }        
     }
 }
